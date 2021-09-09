59f7373863a0151968c76eae8dbf7d38d8517a8b
giuviglia94 <vigliaturo94@gmail.com>
2021-09-09 19:47:14 +0200

    countdwon javascript

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-alert("Hello world");
+const indicatori = document.querySelectorAll('#time span'),
+    scadenza = new Date(2021, 8, 9,19,34,0),
+    ms_scadenza = scadenza.getTime(),
+    ms_giorno = 24*60*60*1000,
+    ms_in_ora= 60*60*1000,
+    ms_in_minuto = 60*1000;
+
+function countdown() {
+    const data_corrente = new Date(),
+        ms_corrente = data_corrente.getTime(),
+        ms_rimanenti = ms_scadenza - ms_corrente;
+
+       
+
+    let giorni = Math.floor(ms_rimanenti/ ms_giorno),
+        ore = Math.floor((ms_rimanenti % ms_giorno) / ms_in_ora),
+        minuti = Math.floor((ms_rimanenti % ms_in_ora) / ms_in_minuto),
+        secondi = Math.floor((ms_rimanenti% ms_in_minuto)/1000);
+    let time = [giorni,ore,minuti,secondi];
+
+     if (ms_rimanenti <= 0 ) {
+            document.getElementById('event').textContent = "Evento iniziato";
+            document.getElementById('time').style.display = "none";
+        }
+        else{
+            indicatori.forEach((span,index) => {
+                span.textContent = time[index]
+                })
+        }
+
+    
+}
+
+const countdownInterval = setInterval(countdown, 1000);
+countdown();
